4bd4ebe4604756ae07b4851d731ee2bb2251e5f6
Yasmin-Alhajjaj <yasmin.alhajjaj@gmail.com>
2019-07-23 11:32:26 +0300

    sol jquery1

--- jQuery-1/jQuery.js
+++ jQuery-1/jQuery.js
@@ -1,49 +1,93 @@
 /*
 Q1:
 Check if jQuery is loaded
-*/
+*/    
+
+$(document).ready(function(){
 
 
+    $("#q1").html("yasmin");
 /*
 Q2:
-If You would have very long website - How to help user with fast getting back on top :)
+If You would have very long website - How to help user with fast getting
+ back on top :)
 */
 
+
+
+    $("#toTop").click(function () {
+   
+        $("html, body").animate({scrollTop:0});
+     });
 
 /*
 Q3:
 If You would like to change sizes of Your div/button with on click :)
 */
+$("#btn1").click(function(){
+    $("#box").animate({
+      height: '100px',
+      width: '100px'
+    });
+  });
 
-
+  $("#btn2").click(function(){
+    $("#box").animate({
+      height: '150px',
+      width: '150px'
+    });
+  });
 /*
 Q4:
 Learn how to fadein your boxes
 */
 
+      $("#btn3").click(function(){
+    $("#div1").delay( 600 ).fadeIn(1000);
+    $("#div2").delay( 1500 ).fadeIn(1000);
+    $("#div3").delay( 100 ).fadeIn(1000);
 
+       });
+
+         
 /*
 Q5:
 User must first accept then he or she can signup
 */
 
+$("#accept").change(function() {
 
+       if($('#accept').prop('checked')) {
+        $("#submitbtn").prop("disabled", false);
+    } else {
+        $("#submitbtn").prop("disabled", true);
+    }
+});
+       
 /*
 Q6:
 Let them print
 */
+
+$('.printPage').click(function(){
+    window.print();
+   // return false;
+}); 
+
 
 
 /*
 Q7:
 Can username be too long? Yest it can - lets limit that
 */
+$("#textarea").attr('maxlength','20');
 
 
 /*
 Q8:
 Wanna make some words bolder, do it with jQuery
 */
+$(".p1").css("font-weight", "bold");
 
 
 /*
@@ -51,17 +95,31 @@ Q9:
 Add new div to your website
 */
 
+$("#t").click(function(){
 
-/*
-Q10:
-Select values from a JSON object using jQuery
-*/
+  $("#t").after("<div>new div</div>")
+ 
+ 
+ })
 
 
 /*
 Q11:
 Add element within ul list
 */
+
+$("#btnclick").click(function(){
+    $("#header li:last").after('<li><a href="#">new li</a></li>');
+
+
+});
+
+
+/*
+Q13:
+Change input value
+*/
+$('#button').attr('value', 'Hello jouza')
 
 
 /*
@@ -71,9 +129,29 @@ Know what user puts into textbox
 
 
 /*
-Q13:
-Change input value
+Q12:
+Know what user puts into textbox
 */
+
+ /* var t=$('#usertext').html($('input:textbox').val());
+
+console.log(t)
+ */
+
+
+
+}); 
+
+/*
+Q10:
+Select values from a JSON object using jQuery
+*/
+
+
+
+
+
+
 
 
 
